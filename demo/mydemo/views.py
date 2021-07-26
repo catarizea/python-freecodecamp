f1a5f8687ffb1fcdd36e9bd84a3a0170d11012c3
Catalin Rizea <catalinrizea1@gmail.com>
2021-07-26 12:33:45 +0300

    model basic usage

--- demo/mydemo/views.py
+++ demo/mydemo/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.details = 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis'
+
+    return render(request, 'index.html', {'feature': feature1})
 
 
 def counter(request):
